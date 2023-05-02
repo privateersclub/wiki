7416606cc079b891db413800f3136d68b9d4b85a
taskylizard <75871323+taskylizard@users.noreply.github.com>
2023-05-02 21:22:09 +0530

    WIP: initialize docs page

---
+++ .vitepress/config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from "vitepress";
+
+export default defineConfig({
+  title: "Wiki",
+  description: "TBD",
+  base: process.env.BASE_URL || "/",
+  themeConfig: {
+    nav: [
+      {
+        text: "Home",
+        link: "/",
+      },
+      {
+        text: "Translations",
+        link: "/translations",
+      },
+    ],
+    search: {
+      provider: "local",
+      options: {
+        // TODO: configure for /translations directory
+        locales: {}
+      }
+    },
+    socialLinks: [
+      { icon: "github", link: "https://github.com/r-piratedgames/wiki" },
+    ],
+  },
+});
---
+++ index.md
@@ -0,0 +1,10 @@
+---
+layout: home
+title: Home
+
+hero:
+  name: Wiki
+  tagline: Welcome to the coolest wiki thing on earth idk TBD
+  actions:
+    - text: Get Started
+      link: /README
