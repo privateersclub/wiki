271f1ac8998d298110b274bd8b84c19b1610a98b
taskylizard <75871323+taskylizard@users.noreply.github.com>
2023-07-30 16:23:39 +0200

    better sidebar
    Signed-off-by: taskylizard <75871323+taskylizard@users.noreply.github.com>

--- docs/sidebar.ts
+++ docs/sidebar.ts
@@ -2,17 +2,22 @@ import fs from "fs";
 import path from "path";
 import matter, { type GrayMatterFile } from "gray-matter";
 import { buildTOC } from "./toc";
+import { getTableOfContents, Items } from "../shared";
 
 const dir = path.join(__dirname, "/wiki/");
 
-function getMetadata(path: string): GrayMatterFile<string> {
+async function getMetadata(
+  path: string,
+): Promise<{ frontmatter: GrayMatterFile<string>; toc: Items }> {
   const contents = fs.readFileSync(path, "utf8");
-  return matter(contents);
+  const frontmatter = matter(contents);
+  const toc = await getTableOfContents(contents);
+  return { frontmatter, toc };
 }
 
 export function generateSidebar(locale: string): any[] {
   let localePath = "";
-  let items: { text: string; link: string }[] = [];
+  let items: { text: string; items: { text: string; link: string }[] }[] = [];
 
   buildTOC();
 
@@ -22,17 +27,20 @@ export function generateSidebar(locale: string): any[] {
     localePath = `${dir}`;
   }
 
-  fs.readdirSync(localePath).forEach((file) => {
+  fs.readdirSync(localePath).forEach(async (file) => {
     const filePath = path.join(localePath, file);
     const stat = fs.statSync(filePath);
     if (stat.isFile() && path.extname(file) === ".md") {
-      const title = getMetadata(filePath).data.title;
-
+      const { frontmatter, toc } = await getMetadata(filePath);
+      const title = frontmatter.data.title;
       if (!title) throw new Error("Cannot find title in metadata: " + filePath);
-
+      const tocItems = toc.items?.map((item) => ({
+        text: item.title,
+        link: `/wiki/${file.replace(/\.md$/, "")}${item.url}`,
+      }));
       items.push({
         text: title,
-        link: `/wiki/${file.replace(/\.md$/, "")}`,
+        items: tocItems!,
       });
     }
   });
