5be584c79f9a968d5bec052e892c27635adb2919
taskylizard <75871323+taskylizard@users.noreply.github.com>
2024-02-10 14:44:57 +0000

    chore: format

--- docs/.vitepress/hooks/Template.vue
+++ docs/.vitepress/hooks/Template.vue
@@ -3,7 +3,9 @@ defineProps<{ title: string; description?: string }>();
 </script>
 
 <template>
-  <div tw="w-full h-full bg-black flex flex-col" style="
+  <div
+    tw="w-full h-full bg-black flex flex-col"
+    style="
       background-image: linear-gradient(
         45deg,
         rgb(14, 14, 25) 0%,
@@ -19,8 +21,11 @@ defineProps<{ title: string; description?: string }>();
         rgb(248, 231, 203) 81%,
         rgb(255, 238, 204) 100%
       );
-    ">
-    <div tw="p-10 w-full min-h-0 grow flex flex-col items-center justify-between">
+    "
+  >
+    <div
+      tw="p-10 w-full min-h-0 grow flex flex-col items-center justify-between"
+    >
       <div tw="w-full flex justify-between items-center text-5xl font-medium">
         <div tw="flex items-center">
           <div tw="text-zinc-100 ml-2 mt-1 font-semibold">
@@ -30,7 +35,11 @@ defineProps<{ title: string; description?: string }>();
       </div>
       <div tw="w-full pr-56 flex flex-col items-start justify-end">
         <div style="color: #eeeeee" tw="text-6xl font-bold" v-html="title" />
-        <div style="color: #adf0dd; text-decoration-style: dotted" tw="mt-2 text-4xl underline" v-html="description" />
+        <div
+          style="color: #adf0dd; text-decoration-style: dotted"
+          tw="mt-2 text-4xl underline"
+          v-html="description"
+        />
       </div>
     </div>
     <div tw="shrink-0 h-4 w-full flex" style="background-color: #adf0dd" />
