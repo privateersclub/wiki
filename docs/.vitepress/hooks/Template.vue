bd829f4d7c42bb5502220fd69bde6bf5c8a694cd
taskylizard <75871323+taskylizard@users.noreply.github.com>
2024-06-02 08:14:28 +0000

    disable ogimg gen for now

--- docs/.vitepress/hooks/Template.vue
+++ docs/.vitepress/hooks/Template.vue
@@ -3,29 +3,29 @@ defineProps<{ title: string; description?: string }>();
 </script>
 
 <template>
-  <div
-    tw="w-full h-full bg-black flex flex-col"
-    style="
+  <div tw="w-full h-full bg-black flex flex-col" style="
       background-image: linear-gradient(
-        45deg,
-        rgb(14, 14, 25) 0%,
-        rgb(29, 33, 43) 19%,
-        rgb(47, 53, 63) 27%,
-        rgb(71, 84, 97) 34%,
-        rgb(101, 122, 133) 41%,
-        rgb(136, 163, 170) 47%,
-        rgb(167, 188, 185) 53%,
-        rgb(192, 203, 191) 59%,
-        rgb(216, 214, 199) 66%,
-        rgb(236, 223, 202) 73%,
-        rgb(248, 231, 203) 81%,
-        rgb(255, 238, 204) 100%
+        43deg,
+        #c6f6d5 1%,
+        #b8f4cc 7%,
+        #aaf2c2 13%,
+        #9ef0ba 19%,
+        #9ae6b4 31%,
+        #8adea7 35.8%,
+        #79d59b 40.6%,
+        #69cd8f 45.4%,
+        #48bb78 55%,
+        #43b072 58.8%,
+        #3ea56c 62.6%,
+        #399a66 66.4%,
+        #2f855a 74%,
+        #2d7b55 78.6%,
+        #2b714f 83.2%,
+        #286749 87.8%,
+        #22543d 97%
       );
-    "
-  >
-    <div
-      tw="p-10 w-full min-h-0 grow flex flex-col items-center justify-between"
-    >
+    ">
+    <div tw="p-10 w-full min-h-0 grow flex flex-col items-center justify-between">
       <div tw="w-full flex justify-between items-center text-5xl font-medium">
         <div tw="flex items-center">
           <div tw="text-zinc-100 ml-2 mt-1 font-semibold">
@@ -35,11 +35,8 @@ defineProps<{ title: string; description?: string }>();
       </div>
       <div tw="w-full pr-56 flex flex-col items-start justify-end">
         <div style="color: #eeeeee" tw="text-6xl font-bold" v-html="title" />
-        <div
-          style="color: #adf0dd; text-decoration-style: dotted"
-          tw="mt-2 text-4xl underline"
-          v-html="description"
-        />
+        <div v-if="description" style="color: #adf0dd; text-decoration-style: dotted" tw="mt-2 text-4xl underline"
+          v-html="description" />
       </div>
     </div>
     <div tw="shrink-0 h-4 w-full flex" style="background-color: #adf0dd" />
